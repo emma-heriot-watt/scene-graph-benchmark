07864b610cd7f5248e134af0cf416e037b81bf13
Amit Parekh <7276308+amitkparekh@users.noreply.github.com>
2023-11-29 16:18:00 +0000

    build: make repo installable more easily

--- setup.py
+++ setup.py
@@ -1,18 +1,19 @@
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
-#!/usr/bin/env python
 
-import glob
 import os
+from pathlib import Path
 
 from setuptools import find_packages
 from setuptools import setup
 
 requirements = ["torch", "torchvision"]
 
-def get_build_extension(): 
+
+def get_build_extension():
     from torch.utils.cpp_extension import BuildExtension
 
     return BuildExtension
+
 
 def get_extensions():
     import torch
@@ -20,12 +21,12 @@ def get_extensions():
     from torch.utils.cpp_extension import CppExtension
     from torch.utils.cpp_extension import CUDAExtension
 
-    this_dir = os.path.dirname(os.path.abspath(__file__))
-    extensions_dir = os.path.join(this_dir, "maskrcnn_benchmark", "csrc")
+    this_dir = Path(__file__).parent
 
-    main_file = glob.glob(os.path.join(extensions_dir, "*.cpp"))
-    source_cpu = glob.glob(os.path.join(extensions_dir, "cpu", "*.cpp"))
-    source_cuda = glob.glob(os.path.join(extensions_dir, "cuda", "*.cu"))
+    extensions_dir = this_dir.joinpath("maskrcnn_benchmark", "csrc")
+    main_file = list(extensions_dir.glob("*.cpp"))
+    source_cpu = list(extensions_dir.glob("cpu/*.cpp"))
+    source_cuda = list(extensions_dir.glob("cuda/*.cu"))
 
     sources = main_file + source_cpu
     extension = CppExtension
@@ -33,7 +34,9 @@ def get_extensions():
     extra_compile_args = {"cxx": []}
     define_macros = []
 
-    if (torch.cuda.is_available() and CUDA_HOME is not None) or os.getenv("FORCE_CUDA", "0") == "1":
+    if (torch.cuda.is_available() and CUDA_HOME is not None) or os.getenv(
+        "FORCE_CUDA", "0"
+    ) == "1":
         extension = CUDAExtension
         sources += source_cuda
         define_macros += [("WITH_CUDA", None)]
@@ -44,9 +47,8 @@ def get_extensions():
             "-D__CUDA_NO_HALF2_OPERATORS__",
         ]
 
-    sources = [os.path.join(extensions_dir, s) for s in sources]
-
-    include_dirs = [extensions_dir]
+    sources = [str(s.relative_to(this_dir)) for s in sources]
+    include_dirs = [str(extensions_dir)]
 
     ext_modules = [
         extension(
@@ -67,7 +69,7 @@ setup(
     author="fmassa",
     url="https://github.com/facebookresearch/maskrcnn-benchmark",
     description="object detection in pytorch",
-    packages=find_packages(exclude=("configs", "tests",)),
+    packages=find_packages(exclude=("configs", "tests")),
     install_requires=requirements,
     ext_modules=get_extensions(),
     cmdclass={"build_ext": get_build_extension()},
